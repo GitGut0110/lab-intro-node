27180b4cb642dc033f9909a99707717368426377
gitgut0110 <jsimms0110@gmail.com>
2018-08-21 17:03:45 +0200

    tests done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,34 @@
-var SortedList = function() {};
-
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+var SortedList = function() {
+  this.items = [];
+  this.length = this.items.length;
+};
+SortedList.prototype.add = function(item) {
+  this.items.push(item);
+  this.length = this.items.length;
+  this.items.sort(function(a, b) {
+    return a - b;
+  });
+};
+SortedList.prototype.get = function(pos) {
+  return this.items[pos - 1];
+};
+SortedList.prototype.max = function() {
+  return this.items[this.items.length - 1];
+};
+SortedList.prototype.min = function() {
+  return this.items[0];
+};
+SortedList.prototype.average = function() {
+  var Sum = this.items.reduce(function(summed, currentValue) {
+    return summed + currentValue;
+  }, 0);
+  return Sum / this.length;
+};
+SortedList.prototype.sum = function() {
+  var Sum = this.items.reduce(function(summed, currentValue) {
+    return summed + currentValue;
+  }, 0);
+  return Sum;
+};
 
 module.exports = SortedList;
